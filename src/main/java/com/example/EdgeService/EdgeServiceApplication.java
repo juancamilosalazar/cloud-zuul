0080ca798c2e5772619c79a99c99436e5d56ee02
camilo <juan.salazar@techandsolve.com>
2019-06-17 13:44:00 -0500

    cambio host conectar eureka

--- src/main/java/com/example/EdgeService/EdgeServiceApplication.java
+++ src/main/java/com/example/EdgeService/EdgeServiceApplication.java
@@ -1,10 +1,18 @@
 package com.example.EdgeService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
